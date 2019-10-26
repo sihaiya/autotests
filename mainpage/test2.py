3efd2cd246196b9d956171f1dae3038bf967835c
Ekaterina <anton.zylyov@gmail.com>
2019-10-26 19:05:46 +0200

    Some some failing tests

---
+++ mainpage/test2.py
@@ -0,0 +1,15 @@
+import pytest
+
+def test_some_feature1():
+    """this test succeeds"""
+    assert True
+
+
+def test_some_feature2():
+    """this test succeeds"""
+    assert True
+
+
+def test_some_fail():
+    """this test FALSE!"""
+    assert False
